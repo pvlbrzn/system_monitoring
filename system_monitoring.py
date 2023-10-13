062fa36c694270cfb34a5e00f5cd029e6d083d2b
Pavel Berezan <pavelberezan1998@gmail.com>
2023-10-13 17:51:32 +0300

    add function process, add comments

--- system_monitoring.py
+++ system_monitoring.py
@@ -1,30 +1,30 @@
 import psutil
 
+#this skript is static version htop
 
-def cpu_info():
+def cpu_info(): # This function returns information about CPU
     res = {}
-    data1 = psutil.cpu_times()
-    data2 = psutil.cpu_freq()
+    data1 = psutil.cpu_times() # This modul outputs info about worktime CPU
+    data2 = psutil.cpu_freq() # This modul outputs info abour frequency CPU
     res.update(user_time=data1.user, 
                sys_time=data1.system, 
                idle_time=data1.idle, 
                guest_time=data1.guest,
                current=data2.current)
-    
     return res
 
-def memory_info():
+def memory_info(): # This function returns information about your memory
     res = {}
-    data = psutil.virtual_memory()
+    data = psutil.virtual_memory() # This modul outputs info about memory
     res.update(total=data.total,
                avail=data.available,
                percent=data.percent,
                free=data.free)
     return res
 
-def net_info():
+def net_info(): # It's function return information about network
     res = {}
-    data = psutil.net_io_counters()
+    data = psutil.net_io_counters() # It's modul output info about info received and sent on the Internet
     res.update(sent=data.bytes_sent,
                recv=data.bytes_recv,
                p_sent=data.packets_sent,
@@ -33,28 +33,34 @@ def net_info():
                errout=data.errout)
     return res
 
-def battary_power_left():
-    battery = psutil.sensors_battery()
+def battary_power_left(): # This function retutn info about battery power left
+    battery = psutil.sensors_battery() 
     power_left = battery.percent
     return power_left
 
-def battary_secsleft():
+def battary_secsleft(): # This function retutns info about battery second left
     battery = psutil.sensors_battery()
     secsleft = battery.secsleft
     return secsleft
 
-def secs2hours(secs):
+def secs2hours(secs): # This function converts time format
     mm, ss = divmod(secs, 60)
     hh, mm = divmod(mm, 60)
     return "%d:%02d:%02d" % (hh, mm, ss)
 
-def battary_power():
+def battary_power(): # This function returns info about battary charging (True/False)
     battery = psutil.sensors_battery()
     bat_power = battery.power_plugged
     return bat_power
 
+def info_process(): #This function returns info about system process
+    res = []
+    for proc in psutil.process_iter(['pid', 'name', 'username']): #enumerate all processes from a module
+        res.append(proc.info)
+    return res
 
-def show(cpu=None, memory=None, net=None, secs=None, charge=None, bpower=None):
+#It's function outputting all transferred functions
+def show(cpu=None, memory=None, net=None, secs=None, charge=None, bpower=None, proc=None):
     cpu_title = ("{:^80}").format("CPU information")
     cpu_sample = ("|{:*^16}"*4 + "|{:*^16}|").format("user_time", 
                                                      "system_time",
@@ -82,7 +88,10 @@ def show(cpu=None, memory=None, net=None, secs=None, charge=None, bpower=None):
     battary_title = ("{:^54}").format("Battery status information")
     bat_par = (("|{:<25}"+"{:>25}|").format(charge, secs))
     electricity = (("|{:<53}|".format(bpower)))
-    processes_title = ("{:^53}").format("All system processes")
+
+    processes_title = ("{:^120}").format("***All system processes***")
+    line = "_"*114
+    
     print("\n\n")
     print(cpu_title)
     print(cpu_sample)
@@ -101,11 +110,11 @@ def show(cpu=None, memory=None, net=None, secs=None, charge=None, bpower=None):
     print(electricity)
     print("\n")
     print(processes_title)
-    for proc in psutil.process_iter():
-        name = proc.name()
-        print(name)
-        if name == "program.exe":
-            pass
+    print(line)
+    for n in proc:
+        output_inf = 'PID {pid:>5} | process name: {name:>50} | user name : {username:>22} |'
+        print(output_inf.format(**n))
+    
 
 def main():
     cpu_data = cpu_info()
@@ -117,12 +126,16 @@ def main():
     battery_secs = "Time left = |{}|".format(secs2hours(bat_sec))
     batpow1 = battary_power()
     batpow = "Power - |{}|".format(batpow1)
+    info_proc = info_process()
+    
+    #call the function show() with all methods(functions)
     show(cpu=cpu_data, 
          memory=memory_data, 
          net=net_data, 
          charge=battery_charge, 
          secs=battery_secs, 
-         bpower=batpow)
+         bpower=batpow,
+         proc=info_proc)
 
 
 if __name__ == "__main__":
